98cb4490b5dd058433dbbc878ebe988be6060b33
SingularDevice <thesingulardevice@gmail.com>
2024-02-11 14:10:42 +0100

    feature: Create product and leaderboard migrations

---
+++ src/infrastructure/persistence/mikro-orm/migration/Migration20240211130601_create_product.ts
@@ -0,0 +1,23 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20240211130601_create_product extends Migration {
+  async up(): Promise<void> {
+    this.addSql(
+      `create table "product" (
+        "id" uuid not null,
+        "author" jsonb not null,
+        "title" varchar(100) not null,
+        "description" varchar(500) not null,
+        "launch_date" date not null,
+        "votes" bigint not null,
+        "country" varchar(3) not null,
+        "created_at" timestampz not null,
+        "updated_at" timestampz not null,
+        constraint "product_pkey" primary key ("id"));`,
+    );
+  }
+
+  async down(): Promise<void> {
+    this.addSql('drop table if exists "product" cascade;');
+  }
+}
