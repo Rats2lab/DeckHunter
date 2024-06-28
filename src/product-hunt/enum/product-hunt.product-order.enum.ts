0fdf4198526f9c225c822ae23abe0a3b1cb3576d
SingularDevice <eduardolopezpardo95@gmail.com>
2024-06-28 13:56:13 +0200

    feat: Implement graphql query filters

---
+++ src/product-hunt/enum/product-hunt.product-order.enum.ts
@@ -0,0 +1,6 @@
+export enum ProductHuntProductOrder {
+  RANKING = 'RANKING',
+  FEATURED_AT = 'FEATURED_AT',
+  NEWEST = 'NEWEST',
+  VOTES = 'VOTES',
+}
--- src/product-hunt/service/product-hunt.find-product.service.ts
+++ src/product-hunt/service/product-hunt.find-product.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { ProductHuntProduct } from '../interface/product-hunt.product.interface';
 import { ProductHuntProductRepository } from '../repository/product-hunt.product.repository';
+import { ProductHuntFindProductsFilters } from '../interface/product-hunt.find-products-filters.interface';
 
 @Injectable()
 export class ProductHuntFindProductService {
   constructor(
     private readonly productHuntProductRepository: ProductHuntProductRepository,
   ) {}
-  async findProduct(): Promise<ProductHuntProduct[]> {
-    return this.productHuntProductRepository.find();
+  async findProduct(
+    findProductsFilters: ProductHuntFindProductsFilters,
+  ): Promise<ProductHuntProduct[]> {
+    return this.productHuntProductRepository.find({
+      postedAfter: findProductsFilters.dateFrom.startOf('day').toISODate(),
+      postedBefore: findProductsFilters.dateTo.startOf('day').toISODate(),
+      order: findProductsFilters.order,
+      featured: String(findProductsFilters.featured),
+    });
   }
 }
